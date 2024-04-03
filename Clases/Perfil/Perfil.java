9e224dad8f169e47ebbd5a85d0f0046b13ba0b60
DiegoIzaguirreLam <130415147+DiegoIzaguirreLam@users.noreply.github.com>
2024-04-02 22:44:27 -0500

    Update Perfil

--- Clases/Perfil/Perfil.java
+++ Clases/Perfil/Perfil.java
@@ -4,7 +4,7 @@ import User.Usuario;
 import Inventario.ObjetoUsable;
 
 public class Perfil {
-    private Usuario usuario;
+	private Usuario usuario;
 	private ArrayList<Expositor> expositores;
 	private ArrayList<Comentario> comentarios;
 	private ArrayList<ObjetoUsable> mostradosPerfil;
