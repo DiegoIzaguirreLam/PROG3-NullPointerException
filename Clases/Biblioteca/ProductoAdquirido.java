b2146edf67386e94cd657f60667c12fa627cae40
DiegoIzaguirreLam <130415147+DiegoIzaguirreLam@users.noreply.github.com>
2024-04-03 00:18:39 -0500

    Update ProductoAdquirido.java

--- Clases/Biblioteca/ProductoAdquirido.java
+++ Clases/Biblioteca/ProductoAdquirido.java
@@ -5,15 +5,15 @@ import java.util.Date;
 import java.util.ArrayList;
 
 public class ProductoAdquirido extends Producto implements Consultable {
-    private Producto producto;
+	private Producto producto;
 	private Date fechaAdquisicion;
-    private Date fechaEjecutado;
-    private LocalTime tiempoUso;
-    private boolean actualizado;
+	private Date fechaEjecutado;
+	private LocalTime tiempoUso;
+	private boolean actualizado;
 	private ArrayList<LogroDesbloqueado> desbloqueados;
 	
-    public void consultarDatos() {
-        // metodo a implementar
-    }
+	public void consultarDatos() {
+		// metodo a implementar
+	}
 
 }
