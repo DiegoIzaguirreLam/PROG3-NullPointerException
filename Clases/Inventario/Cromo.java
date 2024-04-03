52219756481b690faf439b2ddb74fcc9c315f6be
DiegoIzaguirreLam <130415147+DiegoIzaguirreLam@users.noreply.github.com>
2024-04-02 22:54:17 -0500

    Update Cromo.java

--- Clases/Inventario/Cromo.java
+++ Clases/Inventario/Cromo.java
@@ -1,16 +1,5 @@
 package Inventario;
 
-import java.util.*;
-
-/**
- * 
- */
 public class Cromo extends Objeto {
-
-    /**
-     * Default constructor
-     */
-    public Cromo() {
-    }
-
+    
 }
