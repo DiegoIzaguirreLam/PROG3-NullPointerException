255a289b20c759a76fbd13dadcccc76f2434573e
DiegoIzaguirreLam <130415147+DiegoIzaguirreLam@users.noreply.github.com>
2024-04-02 21:56:29 -0500

    Update Proveedor.java

--- Clases/User/Proveedor.java
+++ Clases/User/Proveedor.java
@@ -2,8 +2,8 @@ package User;
 import Biblioteca.Producto;
 
 public class Proveedor extends Usuario {
-    private String nombre;
-    private int nroSeguidores;
+	private String nombre;
+	private int nroSeguidores;
 	private ArrayList<Producto> productosDesarrollados;
 	private ArrayList<Producto> productosPublicados;
 }
