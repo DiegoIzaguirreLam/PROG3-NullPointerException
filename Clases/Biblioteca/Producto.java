26475ae46aa70ee961e36520b4757da17ced7990
DiegoIzaguirreLam <130415147+DiegoIzaguirreLam@users.noreply.github.com>
2024-04-03 00:17:57 -0500

    Update Producto.java

--- Clases/Biblioteca/Producto.java
+++ Clases/Biblioteca/Producto.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import User.Proveedor;
 
 public class Producto {
-    private static int correlativo;
-    private int idProducto;
-    private String titulo;
-    private Date fechaPublicacion;
-    private double precio;
-    private String descripcion;
-    private double espacioDisco;
+	private static int correlativo;
+	private int idProducto;
+	private String titulo;
+	private Date fechaPublicacion;
+	private double precio;
+	private String descripcion;
+	private double espacioDisco;
 	private ArrayList<Proveedor> desarrolladores;
 	private ArrayList<Proveedor> publicadores;
 	
