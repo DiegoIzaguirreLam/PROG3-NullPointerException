46b3d3b0878de91b29f58d312eb981bcf9fa313c
DiegoIzaguirreLam <130415147+DiegoIzaguirreLam@users.noreply.github.com>
2024-04-16 13:29:25 -0500

    Update EtiquetaMySQL.java

--- Proyectos/SteamBibliotecaController/src/pe/edu/pucp/steam/biblioteca/controller/sql/EtiquetaMySQL.java
+++ Proyectos/SteamBibliotecaController/src/pe/edu/pucp/steam/biblioteca/controller/sql/EtiquetaMySQL.java
@@ -4,21 +4,85 @@
  */
 package pe.edu.pucp.steam.biblioteca.controller.sql;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import pe.edu.pucp.steam.biblioteca.controller.dao.EtiquetaDAO;
 import pe.edu.pucp.steam.biblioteca.model.producto.Etiqueta;
 import pe.edu.pucp.steam.biblioteca.model.producto.Producto;
 import pe.edu.pucp.steam.biblioteca.model.producto.ProductoAdquirido;
+import pe.edu.pucp.steam.dbmanager.config.DBManager;
 
 /**
  *
  * @author piero
  */
 public class EtiquetaMySQL implements EtiquetaDAO{
+    private Connection con;
+    private PreparedStatement pst;
+    private CallableStatement cs;
+    private ResultSet rs;
+    
+    @Override
+    public int insertarEtiqueta(Etiqueta etiqueta) {
+        int resultado = 0;
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call INSERTAR_ETIQUETA"
+                    + "(?,?)}");
+            cs.registerOutParameter("_id_etiqueta",
+                    java.sql.Types.INTEGER);
+            cs.setString("_nombre", etiqueta.getNombre());
+            cs.executeUpdate();
+            etiqueta.setIdEtiqueta(cs.getInt("_id_etiqueta"));
+            resultado = etiqueta.getIdEtiqueta();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return resultado;
+    }
 
     @Override
-    public int crearEtiqueta(Etiqueta etiqueta) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public int actualizarEtiqueta(Etiqueta etiqueta) {
+        int resultado = 0;
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call ACTUALIZAR_ETIQUETA"
+                    + "(?,?)}");
+            cs.setInt("_id_etiqueta", etiqueta.getIdEtiqueta());
+            cs.setString("_nombre", etiqueta.getNombre());
+            resultado = cs.executeUpdate();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return resultado;
+    }
+
+    @Override
+    public ArrayList<Etiqueta> listarEtiquetas() {
+        ArrayList<Etiqueta> etiquetas = new ArrayList<>();
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call LISTAR_ETIQUETAS}");
+            rs = cs.executeQuery();
+            while(rs.next()){
+                Etiqueta etiqueta = new Etiqueta();
+                etiqueta.setIdEtiqueta(rs.getInt("id_etiqueta"));
+                etiqueta.setNombre(rs.getString("nombre"));
+                etiquetas.add(etiqueta);
+            }
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return etiquetas;
     }
 
     @Override
@@ -32,8 +96,20 @@ public class EtiquetaMySQL implements EtiquetaDAO{
     }
 
     @Override
-    public int eliminarEtiqueta(Etiqueta etiqueta) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public int eliminarEtiqueta(int idEtiqueta) {
+        int resultado = 0;
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call ELIMINAR_ETIQUETA"
+                    + "(?)}");
+            cs.setInt("_id_etiqueta", idEtiqueta);
+            resultado = cs.executeUpdate();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return resultado;
     }
-    
+
 }
