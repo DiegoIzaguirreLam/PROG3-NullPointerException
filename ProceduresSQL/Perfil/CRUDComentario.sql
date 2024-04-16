fc725dfdd642092fc3205880f781f2b04d73ece4
Sofia <a20210750@pucp.edu.pe>
2024-04-16 12:24:11 -0500

    Update CRUDComentario.sql

--- ProceduresSQL/Perfil/CRUDComentario.sql
+++ ProceduresSQL/Perfil/CRUDComentario.sql
@@ -2,14 +2,15 @@ DROP PROCEDURE IF EXISTS INSERTAR_COMENTARIO;
 DELIMITER $
 CREATE PROCEDURE INSERTAR_COMENTARIO(
 	OUT _id_comentario INT,
-	IN _fid_perfil INT,
-    IN _fid_usuario INT,
+	IN _fid_perfil_comentado INT,
+    IN _fid_usuario_comentarista INT,
 	IN _texto VARCHAR(300),
-	IN _nro_likes INT
+	IN _nro_likes INT,
+    IN _fecha_maxedicion DATE
 )
 BEGIN
-	INSERT INTO Comentario(fid_perfil,fid_usuario,texto,nro_likes, oculto)
-    VALUES(_fid_perfil, _fid_usuario, _texto,_nro_likes,false);
+	INSERT INTO Comentario(fid_perfil_comentado,fid_usuario_comentarista,texto,nro_likes,fecha_publicacion,fecha_maxedicion,oculto)
+    VALUES(_fid_perfil_comentado, _fid_usuario_comentarista, _texto,_nro_likes,CURDATE(),_fecha_maxedicion,false);
     SET _id_comentario = @@last_insert_id;
 END$
 
@@ -17,9 +18,10 @@ DROP PROCEDURE IF EXISTS LISTAR_COMENTARIOS;
 DELIMITER $
 CREATE PROCEDURE LISTAR_COMENTARIOS()
 BEGIN
-    SELECT id_comentario, u.nombre_perfil, texto, nro_likes
+    SELECT id_comentario, u.nombre_perfil as 'autor', p.id_perfil as 'perfil comentado', texto, nro_likes, fecha_publicacion, fecha_maxedicion
     FROM Comentario c
-    INNER JOIN Usuario u ON u.UID = c.fid_usuario
+    INNER JOIN Usuario u ON u.UID = c.fid_usuario_comentarista
+    INNER JOIN Perfil p ON p.id_perfil = c.fid_perfil_comentado
     WHERE oculto = false;
 END$
 
@@ -29,9 +31,9 @@ CREATE PROCEDURE LISTAR_COMENTARIOS_PERFIL(
 	IN _id_perfil INT
 )
 BEGIN
-    SELECT id_comentario, u.nombre_perfil, texto, nro_likes
+    SELECT id_comentario, u.nombre_perfil as 'autor', texto, nro_likes, fecha_publicacion, fecha_maxedicion
     FROM Comentario c
-    INNER JOIN Usuario u ON u.UID = c.fid_usuario
+    INNER JOIN Usuario u ON u.UID = c.fid_usuario_comentarista
     WHERE oculto = false
     AND _id_perfil = fid_perfil_comentarista;
 END$
@@ -44,7 +46,9 @@ CREATE PROCEDURE ACTUALIZAR_COMENTARIO(
     IN _fid_usuario INT,
 	IN _texto VARCHAR(300),
 	IN _nro_likes INT,
-    IN _oculto TINYINT
+    IN _oculto TINYINT,
+    IN _fecha_maxedicion DATE,
+    IN _fecha_publicacion DATE
 )
 BEGIN
     UPDATE Comentario
@@ -52,7 +56,9 @@ BEGIN
 		fid_usuario = _fid_usuario,
         texto = _texto,
         nro_likes = _nro_likes,
-        oculto = _oculto
+        oculto = _oculto,
+        fecha_publicacion = _fecha_publicacion,
+        fecha_maxedicion = _fecha_maxedicion
     WHERE id_comentario = _id_comentario;
 END$
 
