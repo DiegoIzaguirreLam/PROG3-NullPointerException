184ff42c2cc3cdadf4cdae3852fbb51af0947bb5
losfer20 <a20196297@pucp.edu.pe>
2024-04-15 22:43:21 -0500

    SQL Comunidad

---
+++ ProceduresSQL/Comunidad/CRUD_Foro.sql
@@ -0,0 +1,57 @@
+DROP PROCEDURE IF EXISTS CREAR_FORO;
+DELIMITER $ 
+CREATE PROCEDURE CREAR_FORO(
+	OUT _id_foro INT ,
+    IN _nombre VARCHAR(100),
+    IN _descripcion VARCHAR(200),
+    IN _origen_foro VARCHAR(100)
+)
+BEGIN
+	INSERT INTO foro(nombre,descripcion,
+    origen_foro,oculto) VALUES (_nombre,_descripcion,
+    _origen_foro,1);
+	SET _id_foro = @@last_insert_id;
+
+END $
+
+
+DROP PROCEDURE IF EXISTS MOSTRAR_SUBFOROS_POR_FORO;
+DELIMITER $ 
+CREATE PROCEDURE MOSTRAR_SUBFOROS_POR_FORO(
+	in _id_foro INT 
+)
+BEGIN
+	SELECT * FROM subforo WHERE fid_foro = _id_foro 
+    AND oculto = 1;
+
+END $
+
+
+DROP PROCEDURE IF EXISTS EDITAR_FORO;
+DELIMITER $ 
+CREATE PROCEDURE EDITAR_FORO(
+	IN _id_foro INT,
+	IN _nombre VARCHAR(100),
+    IN _descripcion VARCHAR(200),
+    IN _origen_foro VARCHAR(100)
+)
+BEGIN
+	UPDATE foro SET nombre = _nombre,
+    descripcion = _descripcion,
+    origen_foro = _origen_foro 
+    WHERE id_foro = _id_foro; 
+
+END $
+
+DROP PROCEDURE IF EXISTS DESACTIVAR_FORO;
+DELIMITER $ 
+CREATE PROCEDURE DESACTIVAR_FORO(
+	IN _id_foro INT
+)
+BEGIN
+	UPDATE foro SET oculto = 0
+    WHERE id_foro = _id_foro; 
+
+END $
+
+
---
+++ ProceduresSQL/Comunidad/CRUD_mensaje.sql
@@ -0,0 +1,59 @@
+DROP PROCEDURE IF EXISTS CREAR_MENSAJE;
+DELIMITER $ 
+CREATE PROCEDURE CREAR_MENSAJE(
+	OUT _id_mensaje INT,
+    IN _contenido VARCHAR(300),
+    IN _fecha_publicacion DATE,
+    IN _fecha_max_edicion DATE,
+    IN _padre INT,
+    IN _id_hilo INT,
+    IN _id_usuario INT
+)
+BEGIN
+	INSERT INTO mensaje(contenido,fecha_publicacion,
+    fecha_max_edicion,padre,
+    fid_hilo,fid_usuario,oculto)
+    VALUES (_contenido,_fecha_publicacion,
+    _fecha_max_edicion,_padre,
+    _id_hilo,_id_usuario,1);
+	SET _id_mensaje = @@last_insert_id;
+    UPDATE hilo SET nro_mensajes = nro_mensajes +1 
+    WHERE id_hilo = _id_hilo;
+
+END $
+
+DROP PROCEDURE IF EXISTS MOSTRAR_MENSAJE;
+DELIMITER $ 
+CREATE PROCEDURE MOSTRAR_MENSAJE(
+	in _id_mensaje INT 
+)
+BEGIN
+	SELECT * FROM mensaje 
+    WHERE id_mensaje = _id_mensaje 
+    AND oculto = 1;
+
+END $
+
+DROP PROCEDURE IF EXISTS EDITAR_MENSAJE;
+DELIMITER $ 
+CREATE PROCEDURE EDITAR_MENSAJE(
+	IN _id_mensaje INT,
+    IN _contenido VARCHAR(300),
+    IN _fecha_max_edicion DATE
+)
+BEGIN
+	UPDATE mensaje SET contenido = _contenido,
+    fecha_max_edicion = _fecha_max_edicion
+    WHERE id_mensaje = _id_mensaje; 
+END $
+
+DROP PROCEDURE IF EXISTS DESACTIVAR_MENSAJE;
+DELIMITER $ 
+CREATE PROCEDURE DESACTIVAR_MENSAJE(
+	IN _id_mensaje INT
+)
+BEGIN
+	UPDATE mensaje SET oculto = 0
+    WHERE id_mensaje = _id_mensaje; 
+
+END $
