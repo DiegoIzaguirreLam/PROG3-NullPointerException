54b004ce97cb8c3f4156d5c874f154b95a4ca320
losfer20 <a20196297@pucp.edu.pe>
2024-04-15 23:58:53 -0500

    Crud JAVA subForo

--- Proyectos/SteamComunidadController/src/pe/edu/pucp/steam/comunidad/controller/sql/SubforoMySQL.java
+++ Proyectos/SteamComunidadController/src/pe/edu/pucp/steam/comunidad/controller/sql/SubforoMySQL.java
@@ -8,31 +8,138 @@ import java.util.ArrayList;
 import pe.edu.pucp.steam.comunidad.controller.dao.SubforoDAO;
 import pe.edu.pucp.steam.comunidad.model.Hilo;
 import pe.edu.pucp.steam.comunidad.model.Subforo;
+import pe.edu.pucp.steam.dbmanager.config.DBManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.CallableStatement;
 
 /**
  *
  * @author piero
  */
 public class SubforoMySQL implements SubforoDAO{
-
+    
+    private Connection con;
+    private PreparedStatement pst;
+    private CallableStatement cs;
+    private ResultSet rs;
+    
     @Override
-    public int crearSubforo(Subforo subforo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public int crearSubforo(Subforo subforo, int idForo) {
+       int resultado = 0;
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call CREAR_SUBFORO"
+                    + "(?,?)}");
+            cs.registerOutParameter("_id_subforo",
+                    java.sql.Types.INTEGER);
+            cs.setInt("_id_foro", idForo);
+            cs.setString("_nombre",subforo.getNombre());
+      
+            cs.executeUpdate();
+            subforo.setIdSubforo(cs.getInt("_id_subforo"));
+            subforo.setOculto(0);
+            resultado = subforo.getIdSubforo();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            
+            try{con.close();}catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+
+        }
+        return resultado;
     }
 
     @Override
     public ArrayList<Hilo> mostrarHilosSubforo(Subforo subforo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         ArrayList<Hilo> hilos =  new ArrayList<>();
+         
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call MOSTRAR_HILOS_POR_SUBFORO"
+                    + "(?)}");
+ 
+			cs.setInt("_id_subforo", subforo.getIdSubforo());
+            rs = cs.executeQuery();
+			while(rs.next()){
+                Hilo hilo = new Hilo();
+                                boolean b = ((rs.getInt("fijado")) != 0);
+				hilo.setIdHilo(rs.getInt("id_hilo"));
+				hilo.setFijado(b);
+                                hilo.setNroMensajes(rs.getInt("nro_mensajes"));
+                                hilo.setFechaCreacion(rs.getDate("fecha_creacion"));
+                                hilo.setFechaModificacion(rs.getDate("fecha_modificacion"));
+				hilos.add(hilo);
+                
+            }
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            
+            try{con.close();}catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+			try{rs.close();}catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+			
+        }
+        return hilos;   
     }
 
     @Override
     public int editarSubforo(Subforo subforo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         int resultado = 0;
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call EDITAR_SUBFORO"
+                    + "(?,?)}");
+           
+            cs.setInt("_id_subforo", subforo.getIdSubforo());
+            cs.setString("_nombre",subforo.getNombre());
+      
+            resultado = cs.executeUpdate();
+
+            
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            
+            try{con.close();}catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+
+        }
+        return resultado;
     }
 
     @Override
     public int eliminarSubforo(Subforo subforo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int resultado = 0;
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call DESACTIVAR_SUBFORO"
+                    + "(?)}");
+           
+            cs.setInt("_id_subforo", subforo.getIdSubforo());
+      
+            resultado = cs.executeUpdate();
+            subforo.setOculto(1);
+            
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            
+            try{con.close();}catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+
+        }
+        return resultado;
     }
     
 }
