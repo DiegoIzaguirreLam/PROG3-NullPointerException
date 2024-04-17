585e5f624b571ab6d92cc10d237d422b8aa04a0c
Sofia <a20210750@pucp.edu.pe>
2024-04-16 21:02:38 -0500

    Update ExpositorMySQL.java

--- Proyectos/SteamPerfilController/src/pe/edu/pucp/steam/perfil/model/sql/ExpositorMySQL.java
+++ Proyectos/SteamPerfilController/src/pe/edu/pucp/steam/perfil/model/sql/ExpositorMySQL.java
@@ -8,7 +8,10 @@ import pe.edu.pucp.steam.perfil.model.Expositor;
 import pe.edu.pucp.steam.perfil.model.dao.ExpositorDAO;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import pe.edu.pucp.steam.dbmanager.config.DBManager;
+import pe.edu.pucp.steam.perfil.model.Perfil;
 
 /**
  *
@@ -17,15 +20,16 @@ import pe.edu.pucp.steam.dbmanager.config.DBManager;
 public class ExpositorMySQL implements ExpositorDAO{
     private Connection con;
     private CallableStatement cs;
+    private ResultSet rs;
 
     @Override
     public int crearExpositor(Expositor expositor) {
         int resultado = 0;
         try {
-            con = DriverManager.getInstance().getConnection();
+            con = DBManager.getInstance().getConnection();
             cs = con.prepareCall("{call INSERTAR_EXPOSITOR(?,?)}");
             cs.registerOutParameter("_id_expositor", java.sql.Types.INTEGER);
-            cs.setInt("_fid_perfil", expositor.getowner().getIdPerfil());
+            cs.setInt("_fid_perfil", expositor.getOwner().getIdPerfil());
             cs.executeUpdate();
             expositor.setIdExpositor(cs.getInt("_id_expositor"));
             resultado = 1;
@@ -42,12 +46,12 @@ public class ExpositorMySQL implements ExpositorDAO{
     public int actualizaExpositor(Expositor expositor) {
         int resultado = 0;
         try {
-            con = DriverManager.getInstance().getConnection();
+            con = DBManager.getInstance().getConnection();
             cs = con.prepareCall("{call ACTUALIZAR_EXPOSITOR(?,?,?,?)}");
             cs.setInt("_id_expositor", expositor.getIdExpositor());
             cs.setBoolean("_oculto", expositor.getOculto());
             cs.setBoolean("_activo", expositor.getActivo());
-            cs.setInt("_fid_perfil", expositor.getowner().getIdPerfil());
+            cs.setInt("_fid_perfil", expositor.getOwner().getIdPerfil());
             cs.executeUpdate();
             resultado = 1;
             cs.close();
@@ -63,7 +67,7 @@ public class ExpositorMySQL implements ExpositorDAO{
     public int ocultarExpositor(Expositor expositor) {
         int resultado = 0;
         try{
-            con = DriverManager.getInstance().getConnection();
+            con = DBManager.getInstance().getConnection();
             cs = con.prepareCall("{call OCULTAR_EXPOSITOR(?)}");
             cs.setInt("_id_expositor", expositor.getIdExpositor());
             cs.executeUpdate();
@@ -81,7 +85,7 @@ public class ExpositorMySQL implements ExpositorDAO{
     public int eliminarExpositor(Expositor expositor) {
         int resultado = 0;
         try{
-            con = DriverManager.getInstance().getConnection();
+            con = DBManager.getInstance().getConnection();
             cs = con.prepareCall("{call ELIMINAR_EXPOSITOR(?)}");
             cs.setInt("_id_expositor", expositor.getIdExpositor());
             cs.executeUpdate();
@@ -93,5 +97,56 @@ public class ExpositorMySQL implements ExpositorDAO{
             try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());};
         }
         return resultado;
+    }
+
+    @Override
+    public ArrayList<Expositor> listarExpositores() {
+        ArrayList<Expositor> expositores =  new ArrayList<>();
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call LISTAR_EXPOSITORES()}");
+            rs = cs.executeQuery();
+            while(rs.next()){
+                Expositor expositor = new Expositor();
+                expositor.setIdExpositor(rs.getInt("id_expositor"));
+                expositor.setActivo(rs.getBoolean("activo"));
+                expositor.setOculto(rs.getBoolean("oculto"));
+                expositor.setOwner(new Perfil());
+                expositor.getOwner().setIdPerfil(rs.getInt("fid_perfil"));
+                expositores.add(expositor);
+            }
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{rs.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return expositores;
+    }
+
+    @Override
+    public ArrayList<Expositor> listarExpositoresPerfil(int id_perfil) {
+        ArrayList<Expositor> expositores =  new ArrayList<>();
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call LISTAR_EXPOSITORES_PERFIL(?)}");
+            cs.setInt("_id_perfil",id_perfil);
+            rs = cs.executeQuery();
+            while(rs.next()){
+                Expositor expositor = new Expositor();
+                expositor.setIdExpositor(rs.getInt("id_expositor"));
+                expositor.setActivo(rs.getBoolean("activo"));
+                expositor.setOculto(rs.getBoolean("oculto"));
+                expositor.setOwner(new Perfil());
+                expositor.getOwner().setIdPerfil(rs.getInt("fid_perfil"));
+                expositores.add(expositor);
+            }
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{rs.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return expositores;
     }
 }
