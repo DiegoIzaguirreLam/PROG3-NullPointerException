a93af98c5b4bd24a848930baa8d239e57e55fd87
DiegoIzaguirreLam <130415147+DiegoIzaguirreLam@users.noreply.github.com>
2024-04-16 21:21:30 -0500

    Update BibliotecaDAO

--- Proyectos/SteamBibliotecaController/src/pe/edu/pucp/steam/biblioteca/controller/dao/BibliotecaDAO.java
+++ Proyectos/SteamBibliotecaController/src/pe/edu/pucp/steam/biblioteca/controller/dao/BibliotecaDAO.java
@@ -16,7 +16,6 @@ import pe.edu.pucp.steam.biblioteca.model.producto.ProductoAdquirido;
 public interface BibliotecaDAO {
     int crearBiblioteca(Biblioteca biblioteca);
     Biblioteca buscarBiblioteca(int idUser);
-    int actualizarBiblioteca(Biblioteca biblioteca);
     ArrayList<ProductoAdquirido> listarObjetos(Biblioteca biblioteca);
     ArrayList<Coleccion> listarColeccion(int idBiblioteca);
 }
