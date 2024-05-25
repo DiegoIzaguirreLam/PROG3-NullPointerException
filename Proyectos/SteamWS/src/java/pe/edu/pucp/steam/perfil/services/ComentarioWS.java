80b9bbdc38f44b61fecb38c33b47cd8cac5073d7
Piero Pastor <a20210836@pucp.edu.pe>
2024-05-25 11:17:08 -0500

    SERVICIOS TERMINADOS MENOS BIBLIOTECA

--- Proyectos/SteamWS/src/java/pe/edu/pucp/steam/perfil/services/ComentarioWS.java
+++ Proyectos/SteamWS/src/java/pe/edu/pucp/steam/perfil/services/ComentarioWS.java
@@ -8,6 +8,11 @@ package pe.edu.pucp.steam.perfil.services;
 import jakarta.jws.WebService;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
+import java.util.ArrayList;
+import pe.edu.pucp.steam.perfil.dao.ComentarioDAO;
+import pe.edu.pucp.steam.perfil.model.Comentario;
+import pe.edu.pucp.steam.perfil.model.Perfil;
+import pe.edu.pucp.steam.perfil.mysql.ComentarioMySQL;
 
 /**
  *
@@ -17,8 +22,63 @@ import jakarta.jws.WebParam;
 public class ComentarioWS {
 
     /** This is a sample web service operation */
-    @WebMethod(operationName="hello")
-    public String hello(@WebParam(name="name") String txt) {
-        return "Hello "+txt+" !";
+    @WebMethod(operationName="insertarComentario")
+    public int insertarComentario(@WebParam(name="comentario") Comentario comentario) {
+        int resultado=0;
+        try{
+            ComentarioDAO comentarioDao = new ComentarioMySQL();
+            resultado = comentarioDao.insertarComentario(comentario);
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+    
+    @WebMethod(operationName="actualizarComentario")
+    public int actualizarComentario(@WebParam(name="comentario") Comentario comentario) {
+        int resultado=0;
+        try{
+            ComentarioDAO comentarioDao = new ComentarioMySQL();
+            resultado = comentarioDao.actualizarComentario(comentario);
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+    
+    @WebMethod(operationName="ocultarComentario")
+    public int ocultarComentario(@WebParam(name="comentario") Comentario comentario) {
+        int resultado=0;
+        try{
+            ComentarioDAO comentarioDao = new ComentarioMySQL();
+            resultado = comentarioDao.ocultarComentario(comentario);
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+    
+    @WebMethod(operationName="listarComentarios")
+    public ArrayList<Comentario> listarComentarios(){
+        ArrayList<Comentario> comentarios = new ArrayList<>();
+        try{
+            ComentarioDAO comentarioDao = new ComentarioMySQL();
+            comentarios = comentarioDao.listarComentarios();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return comentarios;
+    }
+    
+    @WebMethod(operationName="listarComentariosPerfil")
+    public ArrayList<Comentario> listarComentariosPerfil(@WebParam(name="idPerfil") int idPerfil){
+        ArrayList<Comentario> comentarios = new ArrayList<>();
+        try{
+            ComentarioDAO comentarioDao = new ComentarioMySQL();
+            comentarios = comentarioDao.listarComentariosPerfil(idPerfil);
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return comentarios;
     }
 }
