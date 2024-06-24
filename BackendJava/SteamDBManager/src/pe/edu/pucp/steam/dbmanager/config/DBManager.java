dce34b1baf289774ef18cd4f7340509323778c3f
DiegoIzaguirreLam <izaguirrediego12@gmail.com>
2024-06-23 20:34:56 -0500

    Nueva BD

--- BackendJava/SteamDBManager/src/pe/edu/pucp/steam/dbmanager/config/DBManager.java
+++ BackendJava/SteamDBManager/src/pe/edu/pucp/steam/dbmanager/config/DBManager.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 
 public class DBManager {
     private Connection con;
-    private String url = "jdbc:mysql://db-prog3-nullpointerexception.crrbqj2ohnvo.us-east-1.rds.amazonaws.com:"
-            + "3306/prog3nullpointerexception?useSSL=false";
+    private String url = "jdbc:mysql://db-prog3-final.c3ghkyu94s6g.us-east-1.rds.amazonaws.com:"
+            + "3306/prog3?useSSL=false";
     private String user = "admin";
-    private String password = "prog3nullpointerexception";
+    private String password = "prog3final";
     private static DBManager dbManager;
     
     public static DBManager getInstance(){
